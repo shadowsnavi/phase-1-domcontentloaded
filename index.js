08602c6217cd41f441c32a1f39d41b014fbb2963
shadowsnavi <kumori.k.kuro@gmail.com>
2024-07-31 15:22:34 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+     let initialText = document.querySelector('p').textContent;
+     if (initialText === "JavaScript is so cool. It lets me add text to my page programmatically.") {
+         document.querySelector('p').textContent = "This is really cool!";
+        }
+  });
